b11362d4e00d5a6df9c1759acd06a1a392e9219e
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-04-04 18:28:14 -0500

    security config

---
+++ src/main/java/com/example/propconnectsolo/services/UserDetailsLoader.java
@@ -0,0 +1,28 @@
+package com.example.propconnectsolo.services;
+
+import com.example.propconnectsolo.models.User;
+import com.example.propconnectsolo.models.UserWithRoles;
+import com.example.propconnectsolo.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+    private final UserRepository users;
+
+    public UserDetailsLoader(UserRepository users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = users.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found for " + username);
+        }
+
+        return new UserWithRoles(user);
+    }
+}
