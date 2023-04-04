0651d5d296577b2867f40540f1f3a44deae8ff41
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-04-03 23:06:47 -0500

    user controller register working

--- src/main/java/com/example/propconnectsolo/controllers/UserController.java
+++ src/main/java/com/example/propconnectsolo/controllers/UserController.java
@@ -4,10 +4,11 @@ import com.example.propconnectsolo.models.User;
 import com.example.propconnectsolo.repositories.NoteRepository;
 import com.example.propconnectsolo.repositories.PropertyRepository;
 import com.example.propconnectsolo.repositories.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -15,20 +16,25 @@ public class UserController {
     private final NoteRepository noteDao;
     private final PropertyRepository propDao;
 
-//    private final PasswordEncoder passwordEncoder;
-
     public UserController(UserRepository userDao, NoteRepository noteDao, PropertyRepository propDao) {
         this.userDao = userDao;
         this.noteDao = noteDao;
         this.propDao = propDao;
     }
 
-    @GetMapping("Register")
+    @GetMapping("/register")
     public String signUpForm(Model model){
         model.addAttribute("user", new User());
         return "users/register";
     }
 
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+//        String hash = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/current-weather";
+    }
 
 
 }
