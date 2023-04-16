bd886c496eb1ed6aa1414220f7fa1554e3c70807
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-04-15 22:48:30 -0500

    note controller cont

--- src/main/java/com/example/propconnectsolo/controllers/NoteController.java
+++ src/main/java/com/example/propconnectsolo/controllers/NoteController.java
@@ -4,7 +4,9 @@ import com.example.propconnectsolo.repositories.NoteRepository;
 import com.example.propconnectsolo.repositories.PropertyRepository;
 import com.example.propconnectsolo.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class NoteController {
@@ -22,5 +24,19 @@ public class NoteController {
         this.noteDao = noteDao;
     }
 
-    @GetMapping("notes")
+    @GetMapping("/notes")
+    public String showAllNotes(Model model){
+        model.addAttribute("notes", noteDao.findAll());
+        model.addAttribute("props", propDao.findAll());
+        return "notes/index";
+    }
+
+    @GetMapping("/notes/{id}")
+    public String indNoteShow(@PathVariable long id, Model model){
+        model.addAttribute("notes", noteDao.findById(id));
+        model.addAttribute("users", userDao.findById(id));
+        model.addAttribute("props", propDao.findById(id));
+        return "notes/show";
+    }
+
 }
