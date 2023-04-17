202a7a2cfc42cdd9e2f57006261d4f51066ec937
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-04-16 23:02:49 -0500

    note controller and templates

--- src/main/java/com/example/propconnectsolo/controllers/NoteController.java
+++ src/main/java/com/example/propconnectsolo/controllers/NoteController.java
@@ -1,12 +1,14 @@
 package com.example.propconnectsolo.controllers;
 
+import com.example.propconnectsolo.models.Note;
+import com.example.propconnectsolo.models.Property;
 import com.example.propconnectsolo.repositories.NoteRepository;
 import com.example.propconnectsolo.repositories.PropertyRepository;
 import com.example.propconnectsolo.repositories.UserRepository;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class NoteController {
@@ -24,6 +26,24 @@ public class NoteController {
         this.noteDao = noteDao;
     }
 
+    @GetMapping("notes/create/{id}")
+    public String createNoteForm(@PathVariable long id, Model model){
+        Property property = propDao.findById(id);
+        Note note = new Note();
+        note.setProperty(property);
+        model.addAttribute("note", note);
+        model.addAttribute("propId", id);
+        return "notes/create";
+    }
+
+    @PostMapping("notes/create")
+    public String createNote(@RequestParam(name="propId") long id, @ModelAttribute Note note){
+        Property property = propDao.findById(id);
+        note.setProperty(property);
+        noteDao.save(note);
+        return "redirect:/notes";
+    }
+
     @GetMapping("/notes")
     public String showAllNotes(Model model){
         model.addAttribute("notes", noteDao.findAll());
@@ -37,6 +57,42 @@ public class NoteController {
         model.addAttribute("users", userDao.findById(id));
         model.addAttribute("props", propDao.findById(id));
         return "notes/show";
+    }
+
+    @GetMapping("/notes/search")
+    public String seachResults(@RequestParam(name="q") String query, Model model){
+        model.addAttribute("notes", noteDao.searchByTitleLike(query));
+        return "notes/index";
+    }
+
+    @GetMapping("/notes/{id}/delete")
+    public String confirmDelete(@PathVariable long id, Model model){
+        model.addAttribute("notes", noteDao.findById(id));
+        return "notes/delete";
+    }
+
+    @PostMapping("/notes/{id}/delete")
+    public String deleteNote(@PathVariable long id, @RequestParam(name="note-id") long noteId) {
+        if (id == noteId){
+            Note note = noteDao.findById(id);
+            noteDao.delete(note);
+        }
+        return "redirect:/notes";
+    }
+
+    @GetMapping("/notes/{id}/edit")
+    public String editNoteForm(Model model, @PathVariable long id){
+        Note note = noteDao.findById(id);
+        Property property = propDao.findById(id);
+        note.setProperty(property);
+        model.addAttribute("notes", note);
+        return "notes/edit-note";
+    }
+
+    @PostMapping("/notes/edit")
+    public String editNote(@ModelAttribute Note note){
+        noteDao.save(note);
+        return "redirect:/notes";
     }
 
 }
--- src/main/java/com/example/propconnectsolo/repositories/NoteRepository.java
+++ src/main/java/com/example/propconnectsolo/repositories/NoteRepository.java
@@ -3,6 +3,8 @@ package com.example.propconnectsolo.repositories;
 import com.example.propconnectsolo.models.Note;
 import com.example.propconnectsolo.models.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,5 +12,8 @@ public interface NoteRepository extends JpaRepository<Note, Long> {
     Note findById(long id);
 
     List<Note> findNotesByProperty(Property property);
+
+    @Query("from Note a where a.title LIKE  %:term% OR a.body LIKE  %:term% OR a.category like %:term% OR a.property.city LIKE %:term% OR a.property.state LIKE %:term% OR a.property.street_add LIKE %:term%")
+    List<Note> searchByTitleLike(@Param("term") String term);
 
 }
