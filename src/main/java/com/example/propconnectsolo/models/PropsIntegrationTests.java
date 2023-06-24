6edf1103ec5f9b1031558c2f7ff89546e71928f9
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-06-23 22:10:19 -0500

    sanity test

--- src/main/java/com/example/propconnectsolo/models/PropsIntegrationTests.java
+++ src/main/java/com/example/propconnectsolo/models/PropsIntegrationTests.java
@@ -49,4 +49,17 @@ public class PropsIntegrationTests {
 //                .getRequest()
 //                .getSession();
 //    }
+
+//    @Test
+//    public void contextLoads() {
+//        // Sanity Test, just to make sure the MVC bean is working
+//        assertNotNull(mvc);
+//    }
+//
+//    @Test
+//    public void testIfUserSessionIsActive() throws Exception {
+//        // It makes sure the returned session is not null
+//        assertNotNull(httpSession);
+//    }
+
 }
